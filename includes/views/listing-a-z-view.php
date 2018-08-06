62ff6bd7ba50cb414e994d95033338dbd14dfc1a
MIles Thompson <utunga@gmail.com>
2018-08-06 20:05:13 +1200

    further tweaks

--- includes/views/listing-a-z-view.php
+++ includes/views/listing-a-z-view.php
@@ -12,6 +12,9 @@
     $short_description = $listing->short_description;
     $page_link = $listing->page_link;
     $has_content = $listing->has_content;
+    $has_data_row = !empty($address.$opening_hours.$duration.$contact_name.$phone_number.$email_address);
+    $has_description_row = !empty($short_description);
+    $has_taxonomy_row = $has_content or !empty(post_tags($listing));
 ?>
 
 <div class="directory-item">
@@ -26,30 +29,34 @@
                 printf('%s', $title);  
             } ?>
         </div>
+        <?php
+            if (not_empty($website)) {
+                echo '<div class="item-lg">';  
+                echo sprintf( '<div class="website"><a href="%s" target="_blank">Visit website</a></div>',$website);  
+                echo('</div>'); 
+            }
+        ?> 
     </div>
 
+    <?php if ($has_data_row) { ?>
     <div class="directory-row info-items">
         <?php 
-          if (array_any("not_empty", array( $website))) {
-                echo '<div class="item-lg">';  
-     
-                if (not_empty($website)) {
-                     echo sprintf( '<div class="website"><a href="%s" target="_blank">Visit website</a></div>',$website);  
-                }
-                echo('</div>');  
-            }
             echo itemInDiv($address, "item");
             echo itemsInDiv(array($opening_hours, $duration), "item");
             echo itemsInDiv(array($contact_name, $phone_number, $email_address), "item-sm");
-         
-          
         ?>
     </div>
+    <?php } ?>
+
+    <?php if ($has_description_row) { ?>
     <div class="directory-row">
         <div>
             <?php echo $short_description ?>
         </div>
     </div>
+    <?php } ?>
+ 
+    <?php if ($has_taxonomy_row) { ?>
     <div class="directory-row taxonomy"><?php 
         if ($has_content) 
         {
@@ -66,8 +73,8 @@
                 echo the_post_tags($listing);
             ?>
         </div>
-
     </div>
+    <?php } ?>
 
 </div>
 
